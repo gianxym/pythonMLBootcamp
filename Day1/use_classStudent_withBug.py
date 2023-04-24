67867676457bc7fcda8713d5a8e30bea9b839fd1
APPactivities <drakopoulouevangelia@gmail.com>
2023-04-24 22:55:43 +0300

    material for Day1

---
+++ Day1/use_classStudent_withBug.py
@@ -0,0 +1,6 @@
+from classStudent import Student
+
+student = Student('V', 12, 'Sara')
+print("1st Student: ", student.c, ",", student.x, ",", student.n)
+
+print("call messg function: ", student.messg)
